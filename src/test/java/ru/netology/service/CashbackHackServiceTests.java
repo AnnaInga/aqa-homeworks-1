c5b94426f0d50a206074e0a109844cd151e300f6
AnnaK <anna_morozova_kz@list.ru>
2023-06-21 16:41:53 +0300

    Junit4-platofm

--- src/test/java/ru/netology/service/CashbackHackServiceTests.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTests.java
@@ -52,6 +52,56 @@ public class CashbackHackServiceTests {
 
         assertEquals(expected, actual);
     }
+
+
+    //Jupiter
+    @org.junit.jupiter.api.Test
+
+    public void boundaryThousandJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+
+    public void boundaryZeroJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+
+    public void boundaryLessThousandJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+
+    public void boundaryMoreThousandJupiter() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1100;
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
 }
 
 
